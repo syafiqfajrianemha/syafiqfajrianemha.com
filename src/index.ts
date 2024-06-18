bdbba31cb124abff113e60a3d2ab09666af705b4
Syafiq Fajrian Emha <syafiqfajrianemha@gmail.com>
2024-06-18 12:29:59 +0700

    `fix` : static file ejs

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,8 @@ const port: number = Number(process.env.PORT) ?? 3000;
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static("public"));
+app.use(express.static(__dirname + "/public"));
+app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
 app.use(expressLayouts);
 
